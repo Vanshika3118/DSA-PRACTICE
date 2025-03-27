838c3f003f456c7083103588c15c33330acbc40c
Vanshika <vanshika1455.be22@chitkarauniversity.edu.in>
2025-03-27 19:26:18 +0530

    Sorting Algorithms

--- 1.Array(1D)/Sorting Algorithms/CountingSort.cpp
+++ 1.Array(1D)/Sorting Algorithms/CountingSort.cpp
@@ -1,9 +1,35 @@
 // Use a frequency count of elements from min to max
+// TC : O(n)
 
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 
 int main()
 {
-  
+  int arr[]={64,25,12,22,11};
+  int n=5;
+
+  int freq[100000]={0};
+  int minVal=INT_MAX,maxVal=INT_MIN;
+  for(int i=0;i<n;i++){
+    minVal=min(minVal,arr[i]);
+    maxVal=max(maxVal,arr[i]);
+  }
+  for(int i=0;i<n;i++)
+  {
+    freq[arr[i]]++;
+  }
+  for(int i=minVal,j=0;i<=maxVal;i++)
+  {
+    while(freq[i]>0)
+    {
+      arr[j++]=i;
+      freq[i]--;
+    }
+  }
+  for(int i=0;i<n;i++)
+  {
+    cout<<arr[i]<<" ";
+  }
 }
