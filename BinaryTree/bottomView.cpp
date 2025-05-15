5d814906c9631dae1ad909c74e0dab55a905824b
Vanshika <vanshika1455.be22@chitkarauniversity.edu.in>
2025-05-15 23:06:15 +0530

    Binary Tree

---
+++ BinaryTree/bottomView.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
+using namespace std;
+
+// Definition of Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+public:
+    // Function to return a list of nodes visible from the top view
+    vector<int> topView(Node* root) {
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+
+        queue<pair<Node*, int>> q;
+        map<int, int> mp;
+
+        q.push({root, 0});
+
+        while (!q.empty()) {
+            auto temp = q.front();
+            q.pop();
+            Node* node = temp.first;
+            int line = temp.second;
+
+            //we want to update it in every case
+                mp[line] = node->data;
+
+            if (node->left)
+                q.push({node->left, line - 1});
+            if (node->right)
+                q.push({node->right, line + 1});
+        }
+
+        for (auto it : mp) {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
+};
+
+// Helper function to build a sample tree
+Node* buildSampleTree() {
+    // Example tree:
+    //         1
+    //       /   \
+    //      2     3
+    //       \   / \
+    //        4 5   6
+    //             /
+    //            7
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->right = new Node(4);
+    root->right->left = new Node(5);
+    root->right->right = new Node(6);
+    root->right->right->left = new Node(7);
+    return root;
+}
+
+int main() {
+    Node* root = buildSampleTree();
+    Solution obj;
+    vector<int> result = obj.topView(root);
+
+    cout << "Top View of Binary Tree: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ BinaryTree/rightView.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// Definition of Node
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = right = NULL;
+    }
+};
+
+class Solution {
+public:
+    // Function to return a list of nodes visible from the left view
+    vector<int> leftView(Node* root) {
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+
+        queue<Node*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int n = q.size();
+            for (int i = 0; i < n; i++) {
+                Node* temp = q.front();
+                q.pop();
+
+                // First node of this level
+                if (i == n-1)
+                    ans.push_back(temp->data);
+
+                if (temp->left)
+                    q.push(temp->left);
+                if (temp->right)
+                    q.push(temp->right);
+            }
+        }
+        return ans;
+    }
+};
+
+// Helper function to build a sample tree
+Node* buildSampleTree() {
+    // Example tree:
+    //         1
+    //       /   \
+    //      2     3
+    //     / \     \
+    //    4   5     6
+    //             /
+    //            7
+
+    Node* root = new Node(1);
+    root->left = new Node(2);
+    root->right = new Node(3);
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->right = new Node(6);
+    root->right->right->left = new Node(7);
+    return root;
+}
+
+int main() {
+    Node* root = buildSampleTree();
+    Solution obj;
+    vector<int> result = obj.leftView(root);
+
+    cout << "Right View of Binary Tree: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
